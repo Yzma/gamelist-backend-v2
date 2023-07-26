6c269ac118f6f8e60e029eb9a7ec8175ec4c543f
Yzma <10090913+Yzma@users.noreply.github.com>
2023-07-26 13:12:31 -0400

    Created Genre, Platform and Tag entity classes

---
+++ src/main/java/com/game/gamelist/entity/Tag.java
@@ -0,0 +1,25 @@
+package com.game.gamelist.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity(name = "tags")
+public class Tag {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
