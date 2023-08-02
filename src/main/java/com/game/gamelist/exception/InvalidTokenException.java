6f7a11a84a313e043e24820c8deab670d1db6200
javascriptsucks <evix1988@live.cn>
2023-08-02 01:08:41 -0600

    create invalideTokenexception and invalideAuthorizationException in customizedResponseEntityExceptionHandler

---
+++ src/main/java/com/game/gamelist/exception/InvalidTokenException.java
@@ -0,0 +1,11 @@
+package com.game.gamelist.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class InvalidTokenException extends RuntimeException {
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+}
