3647c0d5e02a1579595f24e114fd68b4933f8e17
javascriptsucks <evix1988@live.cn>
2023-08-04 15:46:08 -0600

    create all method needed in PostServie and PostServiceImpl

--- src/main/java/com/game/gamelist/service/PostService.java
+++ src/main/java/com/game/gamelist/service/PostService.java
@@ -1,4 +1,21 @@
 package com.game.gamelist.service;
 
+import com.game.gamelist.entity.Post;
+import com.game.gamelist.entity.User;
+
+import java.util.Set;
+
 public interface PostService {
+
+    Set<Post> findAllPostsByUserId(User principal);
+
+    Post findPostById(Long requestedId, User principal);
+
+    Post createPost(Post post, User principal);
+
+    Post updatePostById(Long requestedId, Post post, User principal);
+
+    Post deletePostById(Long requestedId, User principal);
+
+    Set<Post> findAllPosts(User principal);
 }
--- src/main/java/com/game/gamelist/service/impl/PostServiceImpl.java
+++ src/main/java/com/game/gamelist/service/impl/PostServiceImpl.java
@@ -1,13 +1,47 @@
 package com.game.gamelist.service.impl;
 
 
+import com.game.gamelist.entity.Post;
+import com.game.gamelist.entity.User;
 import com.game.gamelist.repository.PostRepository;
 import com.game.gamelist.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 @Service
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
     private final PostRepository postRepository;
+
+    @Override
+    public Post updatePostById(Long requestedId, Post post, User principal) {
+        return null;
+    }
+
+    @Override
+    public Post deletePostById(Long requestedId, User principal) {
+        return null;
+    }
+
+    @Override
+    public Set<Post> findAllPosts(User principal) {
+        return null;
+    }
+
+    @Override
+    public Post createPost(Post post, User principal) {
+        return null;
+    }
+
+    @Override
+    public Post findPostById(Long requestedId, User principal) {
+        return null;
+    }
+
+    @Override
+    public Set<Post> findAllPostsByUserId(User principal) {
+        return null;
+    }
 }
