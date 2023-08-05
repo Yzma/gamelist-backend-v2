1473b3d933ccca94e2ca0e075bafa700dfbdb95d
Yzma <10090913+Yzma@users.noreply.github.com>
2023-08-05 15:14:18 -0400

    Implemented getAllNames for Genre, Platform, and Tag repos, as well as getFurthestYear for the Game repo

---
+++ src/main/java/com/game/gamelist/repository/PlatformRepository.java
@@ -0,0 +1,13 @@
+package com.game.gamelist.repository;
+
+import com.game.gamelist.entity.Platform;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface PlatformRepository extends JpaRepository<Platform, Long> {
+
+    @Query("SELECT name FROM platforms")
+    List<String> getAllNames();
+}
