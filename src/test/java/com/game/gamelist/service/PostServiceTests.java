53b78b78a5b14da2f530305a32256b53c86c2c56
javascriptsucks <evix1988@live.cn>
2023-08-09 12:45:16 -0600

    add 2 more tests in PostServiceTests for findAllPosts and deletePostById

--- src/test/java/com/game/gamelist/service/PostServiceTests.java
+++ src/test/java/com/game/gamelist/service/PostServiceTests.java
@@ -5,7 +5,7 @@ import com.game.gamelist.entity.User;
 import com.game.gamelist.repository.PostRepository;
 import com.game.gamelist.repository.UserRepository;
 import com.game.gamelist.service.impl.PostServiceImpl;
-import jakarta.transaction.Transactional;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -18,9 +18,8 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -97,6 +96,51 @@ public class PostServiceTests {
         Assertions.assertThat(updatedPost.getUser()).isEqualTo(updatedUser);
         Assertions.assertThat(updatedPost.getUser()).isNotEqualTo(userToSave);
     }
+    @Test
+    void when_deletePostById_shouldReturn_deletedPost() {
+        // Arrange
+        final var userToSave = User.builder().id(123L).email("changli@gmail.com").username("changli").password("123456").createdAt(LocalDateTime.now()).updatedAt(LocalDateTime.now()).build();
 
+        final var postToSave = Post.builder().id(999L).text("Hello World!").user(userToSave).createdAt(LocalDateTime.now()).updatedAt(LocalDateTime.now()).build();
+
+        when(postRepository.findById(Mockito.anyLong())).thenReturn(java.util.Optional.of(postToSave));
+
+        // Act
+        Post deletedPost = postService.deletePostById(999L, userToSave);
+
+        // Assert
+        Assertions.assertThat(deletedPost).isNotNull();
+        Assertions.assertThat(deletedPost.getId()).isEqualTo(999L);
+        Assertions.assertThat(deletedPost.getText()).isEqualTo("Hello World!");
+        Assertions.assertThat(deletedPost.getUser()).isEqualTo(userToSave);
+    }
+
+    @Test
+    void when_findAllPosts_shouldReturn_allPosts() {
+        // Arrange
+        final var userToSave = User.builder().id(123L).email("changli@gmail.com").username("changli").password("123456").createdAt(LocalDateTime.now()).updatedAt(LocalDateTime.now()).build();
+
+        final var postToSave1 = Post.builder().id(999L).text("Hello World!").user(userToSave).createdAt(LocalDateTime.now()).updatedAt(LocalDateTime.now()).build();
+
+        final var postToSave2 = Post.builder().id(222L).text("Java No.1!").user(userToSave).createdAt(LocalDateTime.now()).updatedAt(LocalDateTime.now()).build();
+
+        final var postToSave3 = Post.builder().id(111L).text("Java No.1?").user(userToSave).createdAt(LocalDateTime.now()).updatedAt(LocalDateTime.now()).build();
+
+        when(postRepository.findAll()).thenReturn(List.of(postToSave1, postToSave2, postToSave3));
+        // Act
+        List<Post> foundPosts = postService.findAllPosts(userToSave);
+        // Assert
+
+        Assertions.assertThat(foundPosts).isNotNull();
+        Assertions.assertThat(foundPosts.size()).isEqualTo(3);
+        Assertions.assertThat(foundPosts.get(0).getId()).isEqualTo(999L);
+        Assertions.assertThat(foundPosts.get(0).getText()).isEqualTo("Hello World!");
+        Assertions.assertThat(foundPosts.get(0).getUser()).isEqualTo(userToSave);
+
+        Assertions.assertThat(foundPosts.get(1).getId()).isEqualTo(222L);
+        Assertions.assertThat(foundPosts.get(1).getText()).isEqualTo("Java No.1!");
+        Assertions.assertThat(foundPosts.get(2).getUser()).isEqualTo(userToSave);
+        Assertions.assertThat(foundPosts.get(2).getId()).isEqualTo(111L);
+    }
 }
 
