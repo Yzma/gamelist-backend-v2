4a526ffd77e9396b28326a985c9b1e1fff5e5da4
javascriptsucks <evix1988@live.cn>
2023-08-20 20:26:16 -0600

    add custom Exception InvalidInputException, implement that on PostService to filter out null input value

--- src/main/java/com/game/gamelist/exception/InvalidInputException.java
+++ src/main/java/com/game/gamelist/exception/InvalidInputException.java
@@ -3,9 +3,9 @@ package com.game.gamelist.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class InvalidTokenException extends RuntimeException {
-    public InvalidTokenException(String message) {
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InvalidInputException extends RuntimeException {
+    public InvalidInputException(String message) {
         super(message);
     }
 }
