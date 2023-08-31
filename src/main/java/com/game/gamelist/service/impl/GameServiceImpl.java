60849042095a8b86fdc4434def0cf6694c3e87bc
Yzma <10090913+Yzma@users.noreply.github.com>
2023-08-31 11:06:02 -0400

    Minor rewrites to GameServiceImpl to allow the 'offset' parameter to be used

--- src/main/java/com/game/gamelist/service/impl/GameServiceImpl.java
+++ src/main/java/com/game/gamelist/service/impl/GameServiceImpl.java
@@ -4,12 +4,15 @@ import com.game.gamelist.dto.GameDTO;
 import com.game.gamelist.entity.Game;
 import com.game.gamelist.mapper.GameMapper;
 import com.game.gamelist.model.GameQueryFilters;
-import com.game.gamelist.repository.GameRepository;
 import com.game.gamelist.service.GameService;
 import com.game.gamelist.specification.GameSpecification;
 import com.game.gamelist.utils.Utils;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -22,8 +25,9 @@ public class GameServiceImpl implements GameService {
     private static final int DEFAULT_QUERY_LIMIT = 10;
     private static final int MIN_QUERY_LIMIT = 1;
     private static final int MAX_QUERY_LIMIT = 35;
-    private final GameRepository gameRepository;
+
     private final GameMapper gameMapper;
+    private final EntityManager em;
 
     @Override
     public List<GameDTO> getAllGames(GameQueryFilters gameQueryFilters) {
@@ -36,11 +40,24 @@ public class GameServiceImpl implements GameService {
         // Ensure a limit is set, we don't want to fetch too many rows
         gameQueryFilters.setLimit(clampLimit(gameQueryFilters.getLimit()));
 
-        Specification<Game> gameSpecification = new GameSpecification(gameQueryFilters);
-        Pageable pageable = Pageable.ofSize(clampLimit(gameQueryFilters.getLimit())).withPage(gameQueryFilters.getOffset());
-        List<Game> foundGames = gameRepository.findAll(gameSpecification, pageable).getContent();
-
+        List<Game> foundGames = getQuery(gameQueryFilters);
         return gameMapper.gamesToGameDTOs(foundGames);
+    }
+
+    private List<Game> getQuery(GameQueryFilters gameQueryFilters) {
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<Game> query = builder.createQuery(Game.class);
+        Root<Game> root = query.from(Game.class);
+
+        Specification<Game> gameSpecification = new GameSpecification(gameQueryFilters);
+
+        query.select(root);
+        query.where(gameSpecification.toPredicate(root, query, builder));
+
+        TypedQuery<Game> typedQuery = em.createQuery(query);
+        typedQuery.setMaxResults(gameQueryFilters.getLimit());
+        typedQuery.setFirstResult(gameQueryFilters.getOffset());
+        return typedQuery.getResultList();
     }
 
     private int clampLimit(int limit) {
