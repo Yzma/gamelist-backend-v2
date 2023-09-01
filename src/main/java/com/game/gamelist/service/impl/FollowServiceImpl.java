45b67f15fee761c504988d7d5163f9b0263d744f
javascriptsucks <evix1988@live.cn>
2023-09-01 11:35:42 -0600

    FollowServiceImpl

--- src/main/java/com/game/gamelist/service/impl/FollowServiceImpl.java
+++ src/main/java/com/game/gamelist/service/impl/FollowServiceImpl.java
@@ -1,2 +1,28 @@
-package com.game.gamelist.service.impl;public class FollowServiceImpl {
+package com.game.gamelist.service.impl;
+
+import com.game.gamelist.entity.User;
+import com.game.gamelist.projection.FollowView;
+import com.game.gamelist.projection.UserBasicView;
+import com.game.gamelist.service.FollowService;
+
+public class FollowServiceImpl implements FollowService {
+    @Override
+    public UserBasicView createFollow(User principle, Long userId) {
+        return null;
+    }
+
+    @Override
+    public UserBasicView removeFollow(User principle, Long userId) {
+        return null;
+    }
+
+    @Override
+    public UserBasicView removeFollower(User principle, Long userId) {
+        return null;
+    }
+
+    @Override
+    public FollowView getAllFollows(User principle) {
+        return null;
+    }
 }
