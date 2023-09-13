65f9da6de1917c074e84ce716981e7649d406a36
Yzma <10090913+Yzma@users.noreply.github.com>
2023-09-13 16:54:19 -0400

    Added indexes to columns that are used for sorting in Game entity

--- src/main/java/com/game/gamelist/entity/Game.java
+++ src/main/java/com/game/gamelist/entity/Game.java
@@ -4,21 +4,51 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
-//@DiscriminatorValue("game_type")
-
 @Getter
 @Setter
 @SuperBuilder
 @NoArgsConstructor
 @Entity(name = "games")
+@Table(indexes = {
+        @Index(name = "nameIndex", columnList = "name"),
+        @Index(name = "releaseDateIndex", columnList = "releaseDate"),
+        @Index(name = "avgScoreIndex", columnList = "avg_score"),
+        @Index(name = "totalRatingIndex", columnList = "total_rating"),
+//        @Index(name = "fn_index", columnList = "firstName"),
+        @Index(name = "mulitIndexNameIdAsc", columnList = "name ASC, id"),
+        @Index(name = "mulitIndexNameIdDesc", columnList = "name DESC, id"),
+
+        @Index(name = "mulitIndexReleaseDateIdAsc", columnList = "releaseDate ASC, id"),
+        @Index(name = "mulitIndexReleaseDateIdDesc", columnList = "releaseDate DESC, id"),
+
+        @Index(name = "mulitIndexAvgScoreIdAsc", columnList = "avg_score ASC, id"),
+        @Index(name = "mulitIndexAvgScoreIdDesc", columnList = "avg_score DESC, id"),
+//        @Index(name = "mulitIndex2", columnList = "lastName, firstName"),
+//        @Index(name = "mulitSortIndex", columnList = "firstName, lastName DESC"),
+//        @Index(name = "uniqueIndex", columnList = "firstName", unique = true),
+//        @Index(name = "uniqueMulitIndex", columnList = "firstName, lastName", unique = true)
+})
 public class Game extends InteractiveEntity {
+
+    /*
+        case "name" -> {
+        case "name_desc" -> query.orderBy(cb.desc(root.get("name")), cb.asc(root.get("id")));
+        case "newest_releases" -> query.orderBy(cb.desc(root.get("releaseDate")), cb.asc(root.get("id")));
+        case "oldest_releases" -> query.orderBy(cb.asc(root.get("releaseDate")), cb.asc(root.get("id")));
+        case "avg_score" -> query.orderBy(cb.desc(root.get("avgScore")), cb.asc(root.get("id")));
+        case "lowest_avg_score" -> query.orderBy(cb.asc(root.get("avgScore")), cb.asc(root.get("id")));
+        case "total_rating" -> query.orderBy(cb.desc(root.get("totalRating")), cb.asc(root.get("id")));
+        case "lowest_total_rating" -> query.orderBy(cb.asc(root.get("totalRating")), cb.asc(root.get("id")));
+    */
 
     private String name;
 
@@ -30,7 +60,7 @@ public class Game extends InteractiveEntity {
     @JsonProperty("cover")
     private String imageURL;
 
-    @Column(name = "`releaseDate`")
+    @Column(name = "releaseDate")
     @JsonProperty("first_release_date")
     private LocalDateTime releaseDate;
 
