6841a7ffcece5040bac777a97ec79be68c7584fe
javascriptsucks <evix1988@live.cn>
2023-09-19 09:35:39 -0600

    create raw sql query in InteractiveEntity

--- src/main/java/com/game/gamelist/repository/InteractiveEntityRepository.java
+++ src/main/java/com/game/gamelist/repository/InteractiveEntityRepository.java
@@ -1,7 +1,21 @@
 package com.game.gamelist.repository;
 
 import com.game.gamelist.entity.InteractiveEntity;
+import com.game.gamelist.projection.PostView;
+import com.game.gamelist.projection.StatusUpdateView;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface InteractiveEntityRepository extends JpaRepository<InteractiveEntity, Long> {
+
+        @Query(value = "SELECT ie.* " +
+                "FROM interactive_entities ie " +
+                "JOIN posts p ON ie.id = p.post_id " +
+                "JOIN status_updates su ON ie.id = su.status_update_id " +
+                "WHERE (p.user_id = :userId OR su.user_id = :userId)" +
+                "ORDER BY ie.created_at DESC", nativeQuery = true)
+        List<InteractiveEntity> findAllPostsAndStatusUpdatesByUserId(Long userId);
 }
